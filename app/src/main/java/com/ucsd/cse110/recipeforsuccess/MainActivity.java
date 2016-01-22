9e01f3dcdf7addda60bd207719304fc2a3b91378
Jason Braness <jbraness@gmail.com>
2016-01-21 23:33:23 -0800

    Android Studio project created from empty activity.  Espresso setup coming later.

---
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ucsd.cse110.recipeforsuccess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
