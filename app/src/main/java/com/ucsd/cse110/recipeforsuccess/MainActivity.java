1e7443c90c0e6f42cc942936787e547ce6b53ec5
crousseaux <constancerousseaux@gmail.com>
2016-01-25 13:22:23 -0800

    The project is now connected to Parse

--- app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
@@ -3,11 +3,22 @@ package com.ucsd.cse110.recipeforsuccess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
