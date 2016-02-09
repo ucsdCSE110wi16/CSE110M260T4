4393d53d2b1eb59f468e5847fff1d9a7496f29ac
Jason Braness <jbraness@gmail.com>
2016-02-09 09:17:17 -0800

    Search now invokes the list activity to display the search results.

--- app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
@@ -1,11 +1,12 @@
 package com.ucsd.cse110.recipeforsuccess;
 
+import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.content.Context;
 import android.support.v7.widget.SearchView;
-import android.widget.TextView;
-
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -13,13 +14,10 @@ import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView m_textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        m_textView = (TextView) findViewById(R.id.textView);
 
         Parse.enableLocalDatastore(this);
 
@@ -31,24 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
 
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                //s is the string that the user submitted to search for
-                m_textView.setText(s);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
-        });
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
         return true;
     }
+
 }
--- app/src/main/java/com/ucsd/cse110/recipeforsuccess/RecipeSearchActivity.java
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/RecipeSearchActivity.java
@@ -3,23 +3,58 @@ package com.ucsd.cse110.recipeforsuccess;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.os.Bundle;
+import android.view.View;
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 
 public class RecipeSearchActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_search);
+        handleIntent(getIntent());
+    }
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
+    private void setListData(String[] searchResults) {
+
+        ArrayAdapter<String> adapter=new
+                ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                searchResults);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             doMySearch(query);
         }
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
     private void doMySearch(String query) {
+
+        //query is the string that the user searched for
+
+        //do the actual search of the database...
+        String[]  fakeSearchResults={"A","B","C"};
+
+        //set list data
+        setListData(fakeSearchResults);
     }
 }
