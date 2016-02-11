b8c5a7219ef201e9ef67b5222ab22f7a4231b011
Jason Braness <jbraness@gmail.com>
2016-02-11 15:49:45 -0800

    Added a search view fragment.  This is displayed while the user is searching.

--- app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/MainActivity.java
@@ -1,16 +1,16 @@
 package com.ucsd.cse110.recipeforsuccess;
 
 import android.app.SearchManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.content.Context;
 import android.support.v7.widget.SearchView;
 import com.parse.Parse;
 import com.parse.ParseObject;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +33,33 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
+        MenuItem searchMenuItem = menu.findItem(R.id.search);
+        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                //Search button clicked display searching fragment view
+                SearchingViewFragment searchingFragment = new SearchingViewFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, searchingFragment)
+                        .commit();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Do whatever you need
+                getSupportFragmentManager().popBackStack();
+
+                SearchingViewFragment searchingFragment = new SearchingViewFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .remove(searchingFragment)
+                        .commit();
+                return true;
+            }
+
+        });
+
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
@@ -41,6 +68,15 @@ public class MainActivity extends AppCompatActivity {
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
