5a7efca083aa4818834e318cf0873bd32666c025
Jason Braness <jbraness@gmail.com>
2016-02-11 20:41:16 -0800

    Added a fragment for recipe details.  Not hooked to anything yet.

---
+++ app/src/main/java/com/ucsd/cse110/recipeforsuccess/RecipeDetailViewFragment.java
@@ -0,0 +1,36 @@
+package com.ucsd.cse110.recipeforsuccess;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecipeDetailViewFragment extends Fragment {
+
+
+
+    public RecipeDetailViewFragment() {
+        // Required empty public constructor
+    }
+
+    public static RecipeDetailViewFragment newInstance(String name) {
+        RecipeDetailViewFragment fragment = new RecipeDetailViewFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_detail_view, container, false);
+    }
+
+}
