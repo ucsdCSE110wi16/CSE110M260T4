9e36cf59cd1f400f93e4c62bae27c35976303809
ellimag <ellimag@gmail.com>
2016-03-04 15:01:35 -0800

    Recipe page fully tested

--- app/src/androidTest/java/com/ucsd/cse110/recipeforsuccess/TestRecipePage.java
+++ app/src/androidTest/java/com/ucsd/cse110/recipeforsuccess/TestRecipePage.java
@@ -1,12 +1,10 @@
 package com.ucsd.cse110.recipeforsuccess;
 
-import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
-import java.lang.Object;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -18,7 +16,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -44,50 +41,33 @@ public class TestRecipePage {
                 .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
 
         // Click the search button
-
-            onView(withId(R.id.button))
+        onView(withId(R.id.button))
                     .perform(click());
 
         Thread.sleep(3000);
 
 
         //Click the first item
-
-            onData(allOf(is(instanceOf(RecipeSearchActivity.MyListItem.class)), is(hasToString("chocolate chip cookies"))))
+        onData(allOf(is(instanceOf(RecipeSearchActivity.MyListItem.class)), is(hasToString("chocolate chip cookies"))))
                     .perform(click());
 
 
 
         //Confirm we got the recipe detail page
-       // onView(withId(R.id.recipeTitle))
-             //   .check(matches(withText(RECIPE_TITLE)));
 
+        onView(allOf(withText(RECIPE_TITLE))).perform(click());
 
-            onView(allOf(withText(RECIPE_TITLE))).perform(click());
-
-
-        // onView(withId(R.id.)).perform(click());
-
-
-
+        //checks all elements are on the recipe page and scrolls the page
         onView(withId(R.id.ratingBar)).check(matches(isDisplayed()));
         onView(withId(R.id.imageView2)).check(matches(isDisplayed()));
         onView(withId(R.id.recipeTitle)).check(matches(isDisplayed()));
         onView(withId(R.id.recipeDetails)).check(matches(isDisplayed())).perform(ViewActions.swipeUp());
-
         onView(withId(R.id.recipeIngredients)).perform(ViewActions.scrollTo());
         Thread.sleep(2000);
         onView(withId(R.id.recipeIngredients)).check(matches(isDisplayed()));
-
         Thread.sleep(2000);
-       onView(withId(R.id.recipeInstructions)).perform(ViewActions.scrollTo());
+        onView(withId(R.id.recipeInstructions)).perform(ViewActions.scrollTo());
         onView(withId(R.id.recipeInstructions)).check(matches(isDisplayed()));
 
-
-
-
-
-       //onView(withId(R.id.recipeTitle))
-       // .check(matches(withText(RECIPE_TITLE)));
     }
 }
