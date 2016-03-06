d7faaa024ed85c5f77cfa5cf32c3f677cf3b7f0d
crousseaux <constancerousseaux@gmail.com>
2016-03-06 12:47:50 -0800

    No MatchesFound Test fixed

--- app/src/androidTest/java/com/ucsd/cse110/recipeforsuccess/NoMatchesFound.java
+++ app/src/androidTest/java/com/ucsd/cse110/recipeforsuccess/NoMatchesFound.java
@@ -11,12 +11,14 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 /**
  *
@@ -52,8 +54,7 @@ public class NoMatchesFound {
             e.printStackTrace();
         }
         //Confirm that the recipe isn't found
-        onView(withId(R.id.recipeTitle))
-                .check(matches(withText(RECIPE_TITLE)));
+        onView(withId(R.id.textView3)).check(matches(isDisplayed()));
 
 
     }
